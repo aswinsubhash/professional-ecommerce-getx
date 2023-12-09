7b999d77a013ddade8703252266e96880f6349a9
Aswin S <aswinofficial3@gmail.com>
2023-12-09 23:46:09 +0530

    Native Splash Added

--- lib/app.dart
+++ lib/app.dart
@@ -8,6 +8,7 @@ class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return  MaterialApp(
+      debugShowCheckedModeBanner: false,
       themeMode: ThemeMode.system,
       theme: AppTheme.lightTheme,
       darkTheme: AppTheme.darkTheme,
