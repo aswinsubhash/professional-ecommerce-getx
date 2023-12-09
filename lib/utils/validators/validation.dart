f0163d9278bd265f072254c8039fa11ee0d5c9e3
Aswin S <aswinofficial3@gmail.com>
2023-12-09 23:04:54 +0530

    Validators added

---
+++ lib/utils/validators/validation.dart
@@ -0,0 +1,55 @@
+class AppValidator {
+  static String? validateEmail(String? value) {
+    if (value == null || value.isEmpty) {
+      return 'Email is required';
+    }
+
+    // Regular expression for email validation
+    final emailRegExp = RegExp(
+        r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+");
+
+    if (!emailRegExp.hasMatch(value)) {
+      return 'Invalid email address.';
+    }
+    return null;
+  }
+
+  static String? validatePassword(String? value) {
+    if (value == null || value.isEmpty) {
+      return 'Password is required.';
+    }
+
+    // Check for minimum password lenght
+    if (value.length < 6) {
+      return 'Password must be at least 6 characters long.';
+    }
+
+    // Check for uppercase letters
+    if (!value.contains(RegExp(r'[A-Z]'))) {
+      return 'Password must contain at least one uppercase letter.';
+    }
+
+    // Check for numbers
+    if (!value.contains(RegExp(r'[0-9]'))) {
+      return 'Password must contain at least one number.';
+    }
+    return null;
+  }
+
+  static String? validatePhoneNumber(String? value) {
+    if (value == null || value.isEmpty) {
+      return 'Phone number is required';
+    }
+
+    // Regular expression for phone number validation
+    final phoneRegExp = RegExp(r'(^(?:[+0]9)?[0-9]{10,12}$)');
+
+    if (!phoneRegExp.hasMatch(value)) {
+      return 'Invalid phone number format';
+    }
+
+    return null;
+  }
+
+  // Add more custom validators as needed for specific requirements.
+}
