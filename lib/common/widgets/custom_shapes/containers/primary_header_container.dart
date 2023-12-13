262ea19743b26f3f1d1d2d6b05ca609e849b6489
Aswin S <aswinofficial3@gmail.com>
2023-12-13 21:41:47 +0530

    Home page clippath added

---
+++ lib/common/widgets/custom_shapes/containers/primary_header_container.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+import '../../../../utils/constants/colors.dart';
+import '../curved_edges/curved_edges_widget.dart';
+import 'circular_container.dart';
+
+class AppPrimaryHeaderContainer extends StatelessWidget {
+  const AppPrimaryHeaderContainer({
+    super.key,
+    required this.child,
+  });
+
+  final Widget child;
+
+  @override
+  Widget build(BuildContext context) {
+    return AppCurvedEdgeWidget(
+      child: Container(
+        color: AppColors.primary,
+        padding: const EdgeInsets.all(0),
+        child: SizedBox(
+          height: 400,
+          child: Stack(
+            children: [
+              /// -- Background Custom Shapes 
+              Positioned(
+                top: -150,
+                right: -250,
+                child: AppCircularContainer(
+                  backgroundColor: AppColors.textWhite.withOpacity(0.1),
+                ),
+              ),
+              Positioned(
+                top: 100,
+                right: -300,
+                child: AppCircularContainer(
+                  backgroundColor: AppColors.textWhite.withOpacity(0.1),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
